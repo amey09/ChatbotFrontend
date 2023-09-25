4807c6d493e939d3fd5aa49201062e43154516d4
amey09 <ameymhatre09@gmail.com>
2023-09-25 23:18:39 +0530

    feat: add reducers to usersSlice
    This commit adds reducers for modifying state
    1. setBooking
    2. setUserDetails

--- src/slices/usersSlice.js
+++ src/slices/usersSlice.js
@@ -9,6 +9,19 @@ const initialState = {
 const userSlice = createSlice({
     name: 'users',
     initialState,
+    reducers: {
+        setBooking: (state, action) => {
+            const newBooking = action.payload;
+            state.userBookings.push(newBooking);
+        },
+        setUserDetails: (state, action) => {
+            const {name, age} = action.payload;
+            state.userName = name
+            state.userAge = age
+        }
+    }
 })
+
+export const {setBooking, setUserDetails} = userSlice.actions;
 
 export default userSlice.reducer;
