fdd1abeb6180bfd0e85613af4dc18a962e6858fb
amey09 <ameymhatre09@gmail.com>
2023-09-26 23:30:11 +0530

    feat: add setMessages to store the messages into store
    This commit stores the unique messages into the store.
    Making it easy for tracking of message during the conversation with chatbot

--- src/chatbot/messageParser.jsx
+++ src/chatbot/messageParser.jsx
@@ -1,17 +1,27 @@
 import React from "react";
+import {useDispatch} from "react-redux";
+import {setMessage} from "../slices/messagesSlice";
 
-function messageParser({ children, actions }) {
-  const parse = (message) => {};
-  return (
-    <div>
-      {React.Children.map(children, (child) => {
-        return React.cloneElement(child, {
-          parse: parse,
-          actions,
-        });
-      })}
-    </div>
-  );
+function MessageParser({children, actions}) {
+    const messages = children.props.state.messages;
+    const dispatch = useDispatch()
+    messages.forEach((message) => {
+        dispatch(setMessage(message));
+    });
+
+    const parse = (message) => {
+    };
+
+    return (
+        <div>
+            {React.Children.map(children, (child) => {
+                return React.cloneElement(child, {
+                    parse: parse,
+                    actions,
+                });
+            })}
+        </div>
+    );
 }
 
-export default messageParser;
+export default MessageParser;
