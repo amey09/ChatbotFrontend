c5e2f16deb4a6797dccc19afbf4ea219d044e946
amey09 <ameymhatre09@gmail.com>
2023-10-06 22:55:59 +0530

    feat: add dashboard component

---
+++ src/components/Dashboard.jsx
@@ -0,0 +1,41 @@
+import {Button, Flex, Heading, VStack, Text} from "@chakra-ui/react";
+import React from "react";
+
+function Dashboard() {
+
+    return (
+        <Flex
+            borderLeft={"5px solid teal"}
+            padding={"0 0.5rem 0 1rem"}
+            boxShadow="xl"
+            flexWrap="nowrap"
+            position="relative"
+        >
+            <VStack alignItems={"left"} flex={1} padding={"0.5rem 0"}>
+                <Heading fontSize={"1.2rem"}>Meeting with Dr.Jyoti</Heading>
+                <Text color={"orange"}>Online</Text>
+                <Text>
+                    demo end date
+                </Text>
+                <Flex justifyContent={"space-between"} alignContent={"center"}>
+                    <Button variant={"link"} colorScheme={"blue"}>
+                        Reschedule
+                    </Button>
+                    <Button variant={"link"} colorScheme={"red"}>
+                        Cancel
+                    </Button>
+                    <Button
+                        colorScheme={"teal"}
+                        size={"sm"}
+                        boxShadow="0px 4px 6px rgba(0, 0, 0, 0.1)"
+
+                    >
+                        New Booking
+                    </Button>
+                </Flex>
+            </VStack>
+        </Flex>
+    );
+}
+
+export default Dashboard;
