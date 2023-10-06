312d0f2c85dbd78505f4045a2d86ba1f8a15bb5a
amey09 <ameymhatre09@gmail.com>
2023-10-06 23:04:07 +0530

    feat: add consume redux store in dashboard component
    This commit hydrates each dashboard component to consume the redux data provided by its parent dashboardScreen

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,8 +1,9 @@
 import {Button, Flex, Heading, VStack, Text} from "@chakra-ui/react";
 import React from "react";
+import {useNavigate} from "react-router-dom";
 
-function Dashboard() {
-
+function Dashboard({eventDate, Time, deleteBooking}) {
+    const navigate = useNavigate();
     return (
         <Flex
             borderLeft={"5px solid teal"}
@@ -15,20 +16,20 @@ function Dashboard() {
                 <Heading fontSize={"1.2rem"}>Meeting with Dr.Jyoti</Heading>
                 <Text color={"orange"}>Online</Text>
                 <Text>
-                    demo end date
+                    {eventDate} - {Time}
                 </Text>
                 <Flex justifyContent={"space-between"} alignContent={"center"}>
                     <Button variant={"link"} colorScheme={"blue"}>
                         Reschedule
                     </Button>
-                    <Button variant={"link"} colorScheme={"red"}>
+                    <Button variant={"link"} colorScheme={"red"} onClick={deleteBooking}>
                         Cancel
                     </Button>
                     <Button
                         colorScheme={"teal"}
                         size={"sm"}
                         boxShadow="0px 4px 6px rgba(0, 0, 0, 0.1)"
-
+                        onClick={() => navigate("/chat-bot")}
                     >
                         New Booking
                     </Button>
