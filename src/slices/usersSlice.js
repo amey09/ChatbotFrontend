631f2ba9cbefe074d25c569687b77c8159873a20
amey09 <ameymhatre09@gmail.com>
2023-10-06 23:13:03 +0530

    feat: add deleteBooking and modify setBooking reducers
    This commit add a new reducer for deleting a booking and modifies the existing setBooking to assign an unique uuid for each session pushed into the redux Store

--- src/slices/usersSlice.js
+++ src/slices/usersSlice.js
@@ -1,27 +1,42 @@
-import {createSlice} from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit";
+import { v4 as uuidv4 } from "uuid";
 
 const initialState = {
-    userName: undefined,
-    userAge: undefined,
-    userBookings: []
-}
+  userName: undefined,
+  userAge: undefined,
+  userBookings: [],
+};
 
 const userSlice = createSlice({
-    name: 'users',
-    initialState,
-    reducers: {
-        setBooking: (state, action) => {
-            const newBooking = action.payload;
-            state.userBookings.push(newBooking);
-        },
-        setUserDetails: (state, action) => {
-            const {name, age} = action.payload;
-            state.userName = name
-            state.userAge = age
-        }
-    }
-})
+  name: "users",
+  initialState,
+  reducers: {
+    setBooking: (state, action) => {
+      const newBooking = {
+        id: uuidv4(),
+        ...action.payload,
+      };
+      state.userBookings.push(newBooking);
+    },
+    deleteBooking: (state, action) => {
+      const bookingId = action.payload;
+      console.log(`Deleting Id initiated: ${bookingId}`)
+      const index = state.userBookings.findIndex(
+        (booking) => booking.id === bookingId
+      );
+      if (index !== -1) {
+        state.userBookings.splice(index, 1);
+      }
+    },
 
-export const {setBooking, setUserDetails} = userSlice.actions;
+    setUserDetails: (state, action) => {
+      const { name, age } = action.payload;
+      state.userName = name;
+      state.userAge = age;
+    },
+  },
+});
+
+export const { setBooking, setUserDetails, deleteBooking } = userSlice.actions;
 
 export default userSlice.reducer;
