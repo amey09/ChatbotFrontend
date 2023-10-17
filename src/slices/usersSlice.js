9c8831803c12929d24a484316d0bfbc6dab51770
amey09 <ameymhatre09@gmail.com>
2023-10-17 23:25:55 +0530

    feat: add resetState reducer to userSlice

--- src/slices/usersSlice.js
+++ src/slices/usersSlice.js
@@ -1,37 +1,38 @@
-import { createSlice } from "@reduxjs/toolkit";
-import { v4 as uuidv4 } from "uuid";
+import {createSlice} from "@reduxjs/toolkit";
 
 const initialState = {
-  sessionInfo: null,
+    sessionInfo: null,
 };
 
 const userSlice = createSlice({
-  name: "customSession",
-  initialState,
-  reducers: {
-    setBooking: (state, action) => {
-      const newBooking = {
-        id: uuidv4(),
-        ...action.payload,
-      };
-      state.sessionInfo.push(newBooking);
+    name: "customSession",
+    initialState,
+    reducers: {
+        setBooking: (state, action) => {
+            const newBooking = {
+                ...action.payload,
+            };
+            state.sessionInfo.push(newBooking);
+        },
+        deleteBooking: (state, action) => {
+            const bookingId = action.payload;
+            const index = state.userBookings.findIndex(
+                (booking) => booking.id === bookingId
+            );
+            if (index !== -1) {
+                state.userBookings.splice(index, 1);
+            }
+        },
+        setUserDetails: (state, action) => {
+            return {...state, sessionInfo: action.payload};
+        },
+        resetState: () => {
+            return initialState;
+        },
     },
-    deleteBooking: (state, action) => {
-      const bookingId = action.payload;
-      console.log(`Deleting Id initiated: ${bookingId}`);
-      const index = state.userBookings.findIndex(
-        (booking) => booking.id === bookingId
-      );
-      if (index !== -1) {
-        state.userBookings.splice(index, 1);
-      }
-    },
-    setUserDetails: (state, action) => {
-      return { ...state, sessionInfo: action.payload };
-    },
-  },
 });
 
-export const { setBooking, setUserDetails, deleteBooking } = userSlice.actions;
+export const {setBooking, setUserDetails, deleteBooking, resetState} =
+    userSlice.actions;
 
 export default userSlice.reducer;
