af601c8fc77335838f6f190e11ef36856975b83c
amey09 <ameymhatre09@gmail.com>
2023-10-17 23:20:46 +0530

    fix: allow scheduleCard to access rescheduledSessionInfo from store
    This commit refactors rescheduledSessions Info to be stored from users => sessionInfo instead of userBookings.

--- src/components/ScheduleCard.jsx
+++ src/components/ScheduleCard.jsx
@@ -1,111 +1,139 @@
-import {Button, IconButton} from "@chakra-ui/button";
+import { Button, IconButton } from "@chakra-ui/button";
 import {
-    Card,
-    CardBody,
-    CardFooter,
-    CardHeader,
-    Flex,
-    HStack,
-    Heading,
-    Text,
+  Card,
+  CardBody,
+  CardFooter,
+  CardHeader,
+  Flex,
+  HStack,
+  Heading,
+  Text,
 } from "@chakra-ui/react";
-import React, {useEffect, useState} from "react";
-import {IoLocationSharp} from "react-icons/io5";
-import {CiTimer} from "react-icons/ci";
-import {AiOutlinePhone, AiOutlineUser} from "react-icons/ai";
-import {useNavigate} from "react-router-dom";
-import {useSelector} from "react-redux";
+import React, { useEffect, useState } from "react";
+import { AiOutlinePhone } from "react-icons/ai";
+import { useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { FaUserAlt, FaLocationArrow } from "react-icons/fa";
+import { BsClock } from "react-icons/bs";
+import { HiOutlineStatusOnline } from "react-icons/hi";
+import { resetState } from "../slices/usersSlice";
 
 const ScheduleCard = () => {
-    const {userBookings} = useSelector((state) => state.users)
-    const {Date, Time, Day} = userBookings[0]
-    const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const { dateTime, wardenName, mode } = useSelector(
+    (state) => state.users.sessionInfo
+  );
 
-    const Stopwatch = () => {
-        const [countdown, setCountdown] = useState(15);
-        useEffect(() => {
-            const interval = setInterval(() => {
-                if (countdown > 0) {
-                    setCountdown((prevCountdown) => prevCountdown - 1)
-                } else {
-                    navigate("/dashboard");
-                    clearInterval(interval);
-                }
-            }, 1000)
-            return () => {
-                clearInterval(interval);
-            };
-        })
-        return (
-            <Flex
-                minHeight={"10svh"}
-                backgroundColor={"teal"}
-                alignItems={"center"}
-                textAlign={"center"}
-            >
-                <Heading size={"md"}>
-                    Redirecting to Dashboard in {countdown} seconds
-                </Heading>
-            </Flex>
-        )
-    }
-
+  const Stopwatch = () => {
+    const [countdown, setCountdown] = useState(15);
+    useEffect(() => {
+      const interval = setInterval(() => {
+        if (countdown > 0) {
+          setCountdown((prevCountdown) => prevCountdown - 1);
+        } else {
+          navigate("/dashboard");
+          clearInterval(interval);
+          dispatch(resetState());
+        }
+      }, 1000);
+      return () => {
+        clearInterval(interval);
+      };
+    });
     return (
-        <>
-            <Card
-                minWidth={"200px"}
-                backgroundColor={"white"}
-                boxShadow="lg"
-                rounded="md"
-                maxHeight={"300px"}
-            >
-                <CardHeader
-                    textColor={"teal.700"}
-                    fontWeight={"600"}
-                    fontSize="lg"
-                    p={"1rem"}
-                >
-                    Session scheduled successfully
-                </CardHeader>
-                <CardBody p={"1rem"}>
-                    <Flex flexDir={"column"} gap={"0.5rem"} width={"100%"}>
-                        <HStack spacing={"1.2rem"}>
-                            <IconButton
-                                aria-label='User Profile Picture'
-                                icon={<AiOutlineUser/>}
-                                borderRadius={"md"}
-                                size={"sm"}
-                            />
-                            <Text fontSize="md">Dr Reddy</Text>
-                        </HStack>
-                        <HStack spacing={"1.2rem"}>
-                            <IconButton aria-label='Booking Details' icon={<CiTimer/>} size={"sm"}/>
-                            <Text fontSize="md"> {Date} {Time} {Day} </Text>
-                        </HStack>
-                        <HStack spacing={"1.2rem"}>
-                            <IconButton aria-label='Location' icon={<IoLocationSharp/>} size={"sm"}/>
-                            <Button variant={"link"} colorScheme="blue">
-                                Open in map
-                            </Button>
-                        </HStack>
-                    </Flex>
-                </CardBody>
-                <CardFooter p={"1rem"}>
-                    <HStack>
-                        <IconButton
-                            aria-label='Customer Support Phone Number'
-                            icon={<AiOutlinePhone/>}
-                            size={"xs"}
-                            colorScheme="teal"
-                        />
-                        <Text fontSize={"xs"}>Need Help? +91 9063293050 (Abhijeet)</Text>
-                    </HStack>
-                </CardFooter>
-                <Stopwatch/>
-            </Card>
-
-        </>
+      <Flex
+        minHeight={"10svh"}
+        backgroundColor={"teal"}
+        alignItems={"center"}
+        textAlign={"center"}
+      >
+        <Heading size={"md"}>
+          Redirecting to Dashboard in {countdown} seconds
+        </Heading>
+      </Flex>
     );
+  };
+
+  return (
+    <>
+      <Card
+        minWidth={"200px"}
+        backgroundColor={"white"}
+        boxShadow="xl"
+        rounded="md"
+        maxHeight={"300px"}
+      >
+        <CardHeader
+          textColor={"teal.700"}
+          fontWeight={"600"}
+          fontSize="lg"
+          p={"1rem"}
+        >
+          Session rescheduled successfully
+        </CardHeader>
+        <CardBody p={"1rem"}>
+          <Flex flexDir={"column"} gap={"0.5rem"} width={"100%"}>
+            <HStack spacing={"1.2rem"}>
+              <IconButton
+                variant={"outline"}
+                aria-label="User Profile Picture"
+                icon={<FaUserAlt />}
+                borderRadius={"md"}
+                size={"sm"}
+              />
+              <Text fontSize="md">Meeting with {wardenName}</Text>
+            </HStack>
+            <HStack spacing={"1.2rem"}>
+              <IconButton
+                variant={"outline"}
+                aria-label="Booking Details"
+                icon={<BsClock />}
+                size={"sm"}
+              />
+              <Text fontSize="md">{dateTime}</Text>
+            </HStack>
+            <HStack spacing={"1.2rem"}>
+              <IconButton
+                variant={"outline"}
+                aria-label="Location"
+                icon={<HiOutlineStatusOnline />}
+                size={"sm"}
+              />
+              <Button variant={"link"} colorScheme="blue">
+                {mode}
+              </Button>
+            </HStack>
+            <HStack spacing={"1.2rem"}>
+              <IconButton
+                variant={"outline"}
+                aria-label="Location"
+                icon={<FaLocationArrow />}
+                size={"sm"}
+              />
+              <Button variant={"link"} colorScheme="blue">
+                Open in map
+              </Button>
+            </HStack>
+          </Flex>
+        </CardBody>
+        <CardFooter p={"1rem"} justifyContent={"center"}>
+          <HStack justifyContent={"space-between"}>
+            <IconButton
+              aria-label="Customer Support Phone Number"
+              icon={<AiOutlinePhone />}
+              size={"xs"}
+              colorScheme="teal"
+            />
+            <Text fontSize={"sm"} color={"blackAlpha.800"} fontWeight={"700"}>
+              Need Help? +91 9063293050 (Abhijeet)
+            </Text>
+          </HStack>
+        </CardFooter>
+        <Stopwatch />
+      </Card>
+    </>
+  );
 };
 
 export default ScheduleCard;
