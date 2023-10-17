86b37ff8bf388dd22fb741b9275c89a1a04203aa
amey09 <ameymhatre09@gmail.com>
2023-10-18 02:40:37 +0530

    chore: prepare the frontend for deployment

--- src/index.js
+++ src/index.js
@@ -6,6 +6,9 @@ import {ChakraProvider, theme} from "@chakra-ui/react";
 import {BrowserRouter} from "react-router-dom";
 import {Provider} from "react-redux";
 import store from "./store";
+import {disableReactDevTools} from "@fvilers/disable-react-devtools";
+
+if (process.env.NODE_ENV === 'production') disableReactDevTools()
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
