4aa17f31a2a3a9acc1fb81c44278009dd23bcd1f
amey09 <ameymhatre09@gmail.com>
2023-10-24 14:50:23 +0530

    fix: explicitly render error instead on Error Component

--- src/screens/DashboardScreen.jsx
+++ src/screens/DashboardScreen.jsx
@@ -1,206 +1,209 @@
 import {
-    Box,
-    Button,
-    Flex,
-    Heading,
-    IconButton,
-    Tab,
-    TabIndicator,
-    TabList,
-    TabPanel,
-    TabPanels,
-    Tabs,
-    useDisclosure,
+  Box,
+  Button,
+  Flex,
+  Heading,
+  IconButton,
+  Tab,
+  TabIndicator,
+  TabList,
+  TabPanel,
+  TabPanels,
+  Tabs,
+  useDisclosure,
 } from "@chakra-ui/react";
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import Dashboard from "../components/Dashboard";
-import {useSelector} from "react-redux";
+import { useSelector } from "react-redux";
 import {
-    useDeleteSessionMutation,
-    useGetSessionsQuery,
+  useDeleteSessionMutation,
+  useGetSessionsQuery,
 } from "../slices/sessionsApiSlice";
 import formatLocalDate from "../utils/dateFormatter";
-import {AddIcon} from "@chakra-ui/icons";
+import { AddIcon } from "@chakra-ui/icons";
 import CreateSessionModal from "../components/CreateSessionModal";
-import {toast, ToastContainer} from "react-toastify";
+import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import ErrorComponent from "../components/ErrorComponent";
 
 const DashboardScreen = () => {
-    const [tabIndex, setTabIndex] = useState(0);
-    const [fetchedSessions, setFetchedSessions] = useState(false);
-    const {data: sessions, isLoading, isError} = useGetSessionsQuery();
+  const [tabIndex, setTabIndex] = useState(0);
+  const [fetchedSessions, setFetchedSessions] = useState(false);
+  const { data: sessions, isLoading, isError } = useGetSessionsQuery();
 
-    const {isWarden} = useSelector((state) => state.auth.userInfo) || {
-        isWarden: false,
-    };
-    const {isOpen, onOpen, onClose} = useDisclosure();
-    const [deleteSession] = useDeleteSessionMutation();
-    const {refetch} = useGetSessionsQuery();
+  const { isWarden } = useSelector((state) => state.auth.userInfo) || {
+    isWarden: false,
+  };
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [deleteSession] = useDeleteSessionMutation();
+  const { refetch } = useGetSessionsQuery();
 
-    useEffect(() => {
-        if (!isLoading && !isError) {
-            setFetchedSessions(true);
-        }
-    }, [isLoading, isError]);
+  useEffect(() => {
+    if (!isLoading && !isError) {
+      setFetchedSessions(true);
+    }
+  }, [isLoading, isError]);
 
+  const handleDeleteBooking = async (sessionID) => {
+    try {
+      await deleteSession({ sessionID }).unwrap();
+      toast.success("Session Deleted", {
+        position: "bottom-left",
+        autoClose: 3000,
+      });
+      refetch();
+    } catch (error) {
+      throw new Error(error);
+    }
+  };
 
-    const handleDeleteBooking = async (sessionID) => {
-        try {
-            await deleteSession({sessionID}).unwrap();
-            toast.success("Session Deleted", {
-                position: "bottom-left",
-                autoClose: 3000,
-            });
-            refetch();
-        } catch (error) {
-            throw new Error(error);
-        }
-    };
+  const handleTabChange = (index) => {
+    setTabIndex(index);
+  };
 
-    const handleTabChange = (index) => {
-        setTabIndex(index);
-    };
-
-    return (
-        <div>
-            {!isError ? (
-                <>
-                    <Flex flexDir={"column"} minHeight={"90vh"}>
-                        <ToastContainer/>
-                        <Tabs
+  return (
+    <div>
+      {!isError ? (
+        <>
+          <Flex flexDir={"column"} minHeight={"90vh"}>
+            <ToastContainer />
+            <Tabs
+              position={"relative"}
+              colorScheme="grey"
+              isLazy={true}
+              index={tabIndex}
+              onChange={handleTabChange}
+            >
+              <TabList
+                justifyContent={"space-around"}
+                position={"sticky"}
+                top={"10svh"}
+                backgroundColor={"white"}
+                zIndex={800}
+              >
+                {sessions &&
+                  typeof sessions === "object" &&
+                  Object.keys(sessions)
+                    .slice(1)
+                    .map((session, index) => <Tab key={index}>{session}</Tab>)}
+              </TabList>
+              <TabIndicator
+                mt="-1.5px"
+                height="2px"
+                bg="blue.500"
+                borderRadius="1px"
+              />
+              <TabPanels>
+                {sessions &&
+                  Object.entries(sessions)
+                    .slice(1)
+                    .map(([sessionName, sessions]) => {
+                      if (Array.isArray(sessions) && sessions.length > 0) {
+                        return (
+                          <TabPanel key={sessionName}>
+                            <Flex flexDir={"column"} gap={"1rem"}>
+                              {sessions.map((session) => (
+                                <Dashboard
+                                  key={session._id}
+                                  sessionName={sessionName}
+                                  wardenName={session.wardenName}
+                                  clientName={session.clientName}
+                                  dateTime={formatLocalDate(session.datetime)}
+                                  rawDate={session.datetime}
+                                  mode={session.mode}
+                                  isWarden={isWarden}
+                                  sessionId={session._id}
+                                  deleteBooking={() =>
+                                    handleDeleteBooking(session._id)
+                                  }
+                                />
+                              ))}
+                            </Flex>
+                          </TabPanel>
+                        );
+                      } else {
+                        return (
+                          <TabPanel
+                            key={sessionName}
+                            justifyContent={"center"}
+                            display={"flex"}
+                            minH={"79.8svh"}
+                            textAlign={"center"}
                             position={"relative"}
-                            colorScheme="grey"
-                            isLazy={true}
-                            index={tabIndex}
-                            onChange={handleTabChange}
-                        >
-                            <TabList
-                                justifyContent={"space-around"}
-                                position={"sticky"}
-                                top={"10svh"}
-                                backgroundColor={"white"}
-                                zIndex={800}
+                          >
+                            <Flex
+                              gap={"1rem"}
+                              flexDir={"column"}
+                              maxW={"fit-content"}
+                              alignItems={"center"}
+                              justifyContent={"center"}
                             >
-                                {sessions &&
-                                    typeof sessions === "object" &&
-                                    Object.keys(sessions)
-                                        .slice(1)
-                                        .map((session, index) => <Tab key={index}>{session}</Tab>)}
-                            </TabList>
-                            <TabIndicator
-                                mt="-1.5px"
-                                height="2px"
-                                bg="blue.500"
-                                borderRadius="1px"
-                            />
-                            <TabPanels>
-                                {sessions &&
-                                    Object.entries(sessions)
-                                        .slice(1)
-                                        .map(([sessionName, sessions]) => {
-                                            if (Array.isArray(sessions) && sessions.length > 0) {
-                                                return (
-                                                    <TabPanel key={sessionName}>
-                                                        <Flex flexDir={"column"} gap={"1rem"}>
-                                                            {sessions.map((session) => (
-                                                                <Dashboard
-                                                                    key={session._id}
-                                                                    sessionName={sessionName}
-                                                                    wardenName={session.wardenName}
-                                                                    clientName={session.clientName}
-                                                                    dateTime={formatLocalDate(session.datetime)}
-                                                                    rawDate={session.datetime}
-                                                                    mode={session.mode}
-                                                                    isWarden={isWarden}
-                                                                    sessionId={session._id}
-                                                                    deleteBooking={() =>
-                                                                        handleDeleteBooking(session._id)
-                                                                    }
-                                                                />
-                                                            ))}
-                                                        </Flex>
-                                                    </TabPanel>
-                                                );
-                                            } else {
-                                                return (
-                                                    <TabPanel
-                                                        key={sessionName}
-                                                        justifyContent={"center"}
-                                                        display={"flex"}
-                                                        minH={"79.8svh"}
-                                                        textAlign={"center"}
-                                                        position={"relative"}
-                                                    >
-                                                        <Flex
-                                                            gap={"1rem"}
-                                                            flexDir={"column"}
-                                                            maxW={"fit-content"}
-                                                            alignItems={"center"}
-                                                            justifyContent={"center"}
-                                                        >
-                                                            <Heading>
-                                                                {isWarden
-                                                                    ? "No Sessions"
-                                                                    : "You have no scheduled sessions"}
-                                                            </Heading>
-                                                            <Button
-                                                                variant={"ghost"}
-                                                                colorScheme={"teal"}
-                                                                size={"sm"}
-                                                                boxShadow="0px 4px 6px rgba(0, 0, 0, 0.1)"
-                                                                maxW={"fit-content"}
-                                                                onClick={() => setTabIndex(1)}
-                                                            >
-                                                                Book a session?
-                                                            </Button>
-                                                        </Flex>
-                                                    </TabPanel>
-                                                );
-                                            }
-                                        })}
-                            </TabPanels>
-                        </Tabs>
-                        <Box
-                            position={"fixed"}
-                            bottom={"1rem"}
-                            left={"50%"}
-                            transform={"translateX(-50%)"}
-                        >
-                            {isWarden ? (
-                                <>
-                                    <IconButton
-                                        zIndex={"100"}
-                                        maxW={"fit-content"}
-                                        variant="outline"
-                                        colorScheme="teal"
-                                        aria-label="Call Sage"
-                                        fontSize="20px"
-                                        icon={<AddIcon/>}
-                                        boxShadow="0px 4px 6px rgba(0, 0, 0, 0.1)"
-                                        onClick={onOpen}
-                                        _hover={{
-                                            shadow: "md",
-                                            transform: "translateY(-5px)",
-                                            transitionDuration: "0.2s",
-                                            transitionTimingFunction: "ease-in-out",
-                                        }}
-                                    />
-                                    <CreateSessionModal
-                                        onClose={onClose}
-                                        onOpen={onOpen}
-                                        isOpen={isOpen}
-                                    />
-                                </>
-                            ) : (
-                                <ErrorComponent/>
-                            )}
-                        </Box>
-                    </Flex>
+                              <Heading>
+                                {isWarden
+                                  ? "No Sessions"
+                                  : "You have no scheduled sessions"}
+                              </Heading>
+                              <Button
+                                variant={"ghost"}
+                                colorScheme={"teal"}
+                                size={"sm"}
+                                boxShadow="0px 4px 6px rgba(0, 0, 0, 0.1)"
+                                maxW={"fit-content"}
+                                onClick={() => setTabIndex(1)}
+                              >
+                                Book a session?
+                              </Button>
+                            </Flex>
+                          </TabPanel>
+                        );
+                      }
+                    })}
+              </TabPanels>
+            </Tabs>
+            <Box
+              position={"fixed"}
+              bottom={"1rem"}
+              left={"50%"}
+              transform={"translateX(-50%)"}
+            >
+              {isWarden ? (
+                <>
+                  <IconButton
+                    zIndex={"100"}
+                    maxW={"fit-content"}
+                    variant="outline"
+                    colorScheme="teal"
+                    aria-label="Call Sage"
+                    fontSize="20px"
+                    icon={<AddIcon />}
+                    boxShadow="0px 4px 6px rgba(0, 0, 0, 0.1)"
+                    onClick={onOpen}
+                    _hover={{
+                      shadow: "md",
+                      transform: "translateY(-5px)",
+                      transitionDuration: "0.2s",
+                      transitionTimingFunction: "ease-in-out",
+                    }}
+                  />
+                  <CreateSessionModal
+                    onClose={onClose}
+                    onOpen={onOpen}
+                    isOpen={isOpen}
+                  />
                 </>
-            ) : null}
-        </div>
-    );
+              ) : (
+                <>
+                  <Heading>
+                    Your browser isn't allowing cookie to be set. Sorry
+                  </Heading>
+                </>
+              )}
+            </Box>
+          </Flex>
+        </>
+      ) : null}
+    </div>
+  );
 };
 
 export default DashboardScreen;
